9e1a951a74d904fa97f6a726668571d24ade27a8
ronsoni123 <rounak.soni2009@gmail.com>
2025-11-09 11:20:26 +0530

    added spring security and oauth2 using keycloak

---
+++ src/main/java/com/example/TaskManager/securityConfig/KeyCloakRoleConverter.java
@@ -0,0 +1,35 @@
+package com.example.TaskManager.securityConfig;
+
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class KeyCloakRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+
+    @Override
+    public Collection<GrantedAuthority> convert(Jwt jwt) {
+        Map<String, Object> realmAccess = jwt.getClaim("realm_access");
+        if (realmAccess == null || realmAccess.isEmpty()) {
+            return List.of();
+        }
+        List<String> roles = (List<String>) realmAccess.get("roles");
+
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                .collect(Collectors.toList());
+    }
+
+    public static JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(new KeyCloakRoleConverter());
+        return converter;
+    }
+}
---
+++ src/main/java/com/example/TaskManager/securityConfig/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.example.TaskManager.securityConfig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http.authorizeHttpRequests(auth -> auth.requestMatchers("/api/v1/tasks/*")
+                .authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(KeyCloakRoleConverter.jwtAuthenticationConverter()))).build();
+    }
+
+
+}
